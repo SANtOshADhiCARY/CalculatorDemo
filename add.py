d75bbf063fd04078a9a45918394402338cd16ad6
SANtOshADhiCARY <adhikarisantosh20551117@gmail.com>
2022-07-16 16:03:05 +0545

    Addition Modified 0.0

---
+++ add.py
@@ -0,0 +1,5 @@
+First_Number=int(input("Enter the first Number you want to add: "))
+SecondNumber=int(input("Enter the second Number you want to add: "))
+total=First_Number+SecondNumber
+
+print(total)
