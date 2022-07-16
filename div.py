6c06942ba23783b169fe9ff1bded30b263642399
SANtOshADhiCARY <adhikarisantosh20551117@gmail.com>
2022-07-16 16:26:03 +0545

    DIV/MUL/SUB Updated 0.1

---
+++ div.py
@@ -0,0 +1,6 @@
+FirstNumber=int(input("Enter the first number: "))
+SecondNumber=int(input("Enter the second number: "))
+
+total=FirstNumber/SecondNumber
+
+print(total)
---
+++ sub.py
@@ -0,0 +1,6 @@
+FirstNumber=int(input("Enter the First Number: "))
+SecondNumber=int(input("Enter the Second Number: "))
+
+total=FirstNumber-SecondNumber
+
+print(total)
